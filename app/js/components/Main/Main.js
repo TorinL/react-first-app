9a9f8573939d9b5e01204b6930948110a220e5fe
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-11-01 17:07:11 -0700

    added item

--- app/js/components/Main/Main.js
+++ app/js/components/Main/Main.js
@@ -20,15 +20,16 @@ export default class Main extends React.Component {
 					<h2>Lorem ipsum dolor sit amet</h2>
 					<ul>
 						<li>Fusce rutrum nunc vitae</li>
+						<li> Adding list item for spacing.</li>
 						<li>Morbi a augue eu magna ornare tempus.</li>
 						<li>Etiam vulputate mauris vitae commodo sagittis.</li>
 					</ul>
 				</div>
 				<div className={divider} />
 				<ProductProfile id={randomProduct}/>
-				<Link to={`/products/${randomProduct}`}>Click here for more</Link> 
+				<Link to={`/products/${randomProduct}`}>Click here for more</Link>
 				<p>
-					Ut ultricies tellus non orci vehicula finibus. Sed tellus augue, varius et tortor nec, 
+					Ut ultricies tellus non orci vehicula finibus. Sed tellus augue, varius et tortor nec,
 					semper laoreet ex.
 				</p>
 				<div className={divider} />
